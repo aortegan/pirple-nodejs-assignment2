410bc58e553cc87621556004c201e41b8d134edc
Toni Ortega <toni.47.ortega@gmail.com>
2019-02-27 22:07:15 +0530

    First commit
    Readme
    All files needed ara created
    HTTPS cert and key
    Started with users handler and associated helpers/data functions
    Servers can start, but no requests can be performed

---
+++ README.md
@@ -0,0 +1,5 @@
+# pirple-nodejs-assignment2
+
+Second assignment for the NodeJS masterclass from Pirple
+
+## Description
---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+ * Primary file for the API
+ *
+ */
+
+// Dependencies
+var server = require('./lib/server');
+//var workers = require('./lib/workers');
+
+// Declare the application
+let app = {};
+
+// Init function
+app.init = ()=>{
+	// Start the server
+	server.init();
+
+	// Start the workers
+	//workers.init();
+};
+
+// initialize the app
+app.init();
+
+// Export the module app
+module.exports = app;
